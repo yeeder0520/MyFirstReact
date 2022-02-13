1bf808840110964c081fe1c3d46e6760920d09af
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2022-02-13 17:20:17 +0800

    調整 list 滑鼠移動過去會顯示刪除按鈕

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,18 +2,38 @@ import React, { Component } from "react";
 import "./index.css";
 
 export default class Item extends Component {
+  //控制刪除按鍵顯示的狀態
+  state = { showBtn: false };
+
+  //滑鼠進來-顯示"刪除"按鈕
+  handlerMouseOver = () => {
+    this.setState({ showBtn: true });
+  };
+
+  //滑鼠出去-隱藏"刪除"按鈕
+  handlerMouseLeave = () => {
+    this.setState({ showBtn: false });
+  };
+
   render() {
-
     //從List取得測試資料
-    const {id,name,done} = this.props.todos
+    const { id, name, done } = this.props.todos;
 
+    //從List取得"刪除按鈕"顯示與隱藏
+    const { showBtn } = this.state;
     return (
-      <li>
+      <li
+        onMouseOver={this.handlerMouseOver}
+        onMouseLeave={this.handlerMouseLeave}
+      >
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: "none" }}>
+        <button
+          className="btn btn-danger"
+          style={{ display: showBtn ? "block" : "none" }}
+        >
           删除
         </button>
       </li>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -6,10 +6,14 @@ export default class List extends Component {
     //從App取得測試資料
     const todoArray = this.props.todos;
     return (
-      <ul className="todo-main">
+      <ul
+        className="todo-main"
+        onMouseOver={this.handlerMouseOver}
+        onMouseLeave={this.handlerMouseLeave}
+      >
         {todoArray &&
           todoArray.map((todo) => {
-            return <Item key={todo.id} todos={todo}/>;
+            return <Item key={todo.id} todos={todo} />;
           })}
       </ul>
     );
